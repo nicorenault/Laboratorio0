7e8ef6a3295366e07bf209add71846a17fb04e36
Caro0025 <caimicarolina@gmail.com>
2025-03-19 10:40:31 -0300

    Update DTRefer.h

--- INCLUDE/DTRefer.h
+++ INCLUDE/DTRefer.h
@@ -2,13 +2,16 @@
 #define DTREFER
 #include "DTFecha.h"
 #include <string>
+#include <vector>
+#include <iostream>
 using namespace std;
+
 class DTRefer {
     private:
         string doi;
         string titulo;
         DTFecha fecha;
-        //autores set string creo que se refiere a un arreglo de strings de autores pero no estoy seguro
+        vector<std::string> autores; 
     public:
         DTRefer(const string& doi_, const string& titulo_, const DTFecha& fecha_);
         ~DTRefer();  
@@ -17,7 +20,9 @@ class DTRefer {
         string getTitulo();
         DTFecha getFecha();
         int getNumAutores();
-  
-    };
+        vector<std::string> getAutores();
+        friend std::ostream& operator<<(std::ostream& os, const DTRefer& dt); // A esto se refiere la parte 2. Más eficiente la impresión del objeto
+
+};
     
 #endif
