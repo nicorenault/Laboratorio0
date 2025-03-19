572ea9503a3086ffb770e515e2c69dc3ba4b35e4
Caro0025 <caimicarolina@gmail.com>
2025-03-19 10:52:02 -0300

    Update Libro.h

--- INCLUDE/Libro.h
+++ INCLUDE/Libro.h
@@ -5,17 +5,22 @@
 
 #include "Publicacion.h"
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Libro : public virtual Publicacion{
 	private:
 		string Editorial;
+		vector<std::string> PalabrasDestacadas;
 	public:
 	Libro();
-	Libro(const string& Editorial_);
-  	virtual ~Libro();	
+	Libro(const string& Editorial_; vector<std::string> PalabrasDestacadas);
+  	virtual ~Libro();
+	void setEditorial(string editorial);
+    	void setPalabrasDestacadas(string PalabrasDestacadas);
+    	string getEditorial();
+	string getPalabrasDestacadas();
 	virtual bool ContienePalabra(string Palabra);
-//string PalabrasDestacadas();?
 };
 #endif
