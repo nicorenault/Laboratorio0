8825c5255edcc048c4ce360cc2c126437a3c67bd
Caro0025 <caimicarolina@gmail.com>
2025-03-19 10:53:46 -0300

    Update PaginaWeb.h

--- INCLUDE/PaginaWeb.h
+++ INCLUDE/PaginaWeb.h
@@ -4,16 +4,22 @@
 
 #include "Publicacion.h"
 #include <string>
+
 using namespace std;
+
 class PaginaWeb : public virtual Publicacion{
 	private:
 		string url;
 		string ContenidoExtraido;
 	public:
-        PaginaWeb();
-        PaginaWeb(const string& url_, const string& ContenidoExtraido_);   
-        virtual ~PaginaWeb(); 
-
-	    virtual bool ContienePalabra(const string& Palabra);
+        	PaginaWeb();
+        	PaginaWeb(const string& url_, const string& ContenidoExtraido_);   
+        	virtual ~PaginaWeb(); 
+		void setUrl(std::string url);
+    		void setcontenidoExtraido(std::string contenidoExtraido);		
+    		string getUrl();
+    		string getcontenidoExtraido();
+	    	virtual bool ContienePalabra(const string& Palabra);
 };
+
 #endif
