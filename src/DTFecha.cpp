3b8afe4864e9171ec1582a30e7aea10d26e23a64
camimirci <camilamirci06@icloud.com>
2025-03-20 16:38:32 -0300

    Update DTFecha.cpp

--- src/DTFecha.cpp
+++ src/DTFecha.cpp
@@ -1,1 +1,29 @@
+#include "DTFecha.h"
 
+DTFecha::DTFecha(int d, int m, int a) : dia(d), mes(m), anio(a) {}
+
+// Getters
+int DTFecha::getDia() const {
+    return dia;
+}
+
+int DTFecha::getMes() const {
+    return mes;
+}
+
+int DTFecha::getAnio() const {
+    return anio;
+}
+ 
+// Setters
+void DTFecha::setDia(int d) { 
+    this->dia = d; 
+}
+
+void DTFecha::setMes(int m) { 
+    this->mes = m; 
+}
+
+void DTFecha::setAnio(int a) { 
+    this->anio = a; 
+}
