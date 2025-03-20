96f76b96901b6eff9634467a081fc7b4778784bb
vanemercader <vanessa.mercader@gmail.com>
2025-03-20 17:02:43 -0300

    Update DTFecha.cpp

--- src/DTFecha.cpp
+++ src/DTFecha.cpp
@@ -19,3 +19,17 @@ int DTFecha::getAnio() const {
     return anio;
 }
  
+// Método toString para representar la fecha como string
+string DTFecha::toString() const {
+    ostringstream oss;
+    oss << dia << "/" << mes << "/" << anio;
+    return oss.str();
+}
+
+// Operador de comparación '>'
+bool DTFecha::operator>(const DTFecha& otra) const {
+    if (anio > otra.anio) return true;
+    if (anio == otra.anio && mes > otra.mes) return true;
+    if (anio == otra.anio && mes == otra.mes && dia > otra.dia) return true;
+    return false;
+}
