7af8abc07fdb155113eac0be9c0bbbadd310f9e6
nicorenault <81483423+nicorenault@users.noreply.github.com>
2025-03-20 18:45:07 -0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include "DTFecha.h"
 #include "ArticuloRevista.h"
 #include "Libro.h"
@@ -10,12 +9,18 @@
 using namespace std;
 
 int main() {
-    // a. Crear objetos de ArticuloRevista
     ArticuloRevista* art1 = new ArticuloRevista(
         "10.1234/abc123", 
         "Fundamentos de POO", 
         DTFecha(15, 5, 2023), 
         "Programacion Avanzada", 
         "Introduccion a los principios fundamentales de la programacion orientada a objetos, explicando sus conceptos clave como clases, objetos, herencia y polimorfismo."
+    );
+        ArticuloRevista* art2 = new ArticuloRevista(
+        "10.4567/jkl012", 
+        "Utilidad de diagramas UML", 
+        DTFecha(10, 2, 2024), 
+        "Modelado de Software", 
+        "Ejercicio empirico de como los diagramas UML pueden ayudar en el proceso y documentacion de software, cubriendo los tipos mas importantes utilizados, como clases."
     );
 }
