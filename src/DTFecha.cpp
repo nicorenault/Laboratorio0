8710a1f8e2a37130b7373c6e08b40de4c308510f
mateosalgado10 <mateosalgado100804@gmail.com>
2025-03-20 16:44:42 -0300

    Update DTFecha.cpp

--- src/DTFecha.cpp
+++ src/DTFecha.cpp
@@ -1,1 +1,27 @@
+#include "DTFecha.h"
 
+// Constructor con parámetros
+DTFecha::DTFecha(int d, int m, int a){
+    dia = d;
+    mes = m;
+    anio = a;
+}
+
+// Destructor
+DTFecha::~DTFecha() {
+}
+
+// Getters
+void DTFecha::getDia(int d) {
+    this->dia = d;
+}
+
+void DTFecha::getMes(int a) {
+    this->mes = m;
+}
+
+void DTFecha::getAnio(int a) {
+    this->anio = a;
+}
+
+//
