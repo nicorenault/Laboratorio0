0ab264c758b1817e22a0e103bb11c0c838a1a952
camimirci <camilamirci06@icloud.com>
2025-03-20 16:42:06 -0300

    Update Investigador.cpp

---
+++ src/Investigador.cpp
@@ -0,0 +1,39 @@
+#include "Investigador.h"
+#include <iostream>
+
+// Constructor
+Investigador::Investigador(const string& orcid, const string& nombre, const string& institucion)
+    : ORCID(orcid), Nombre(nombre), Institucion(institucion) {}
+
+// Destructor
+Investigador::~Investigador() {}
+
+// Getters
+string Investigador::getORCID() const { 
+    return ORCID; 
+}
+
+string Investigador::getNombre() const { 
+    return Nombre; 
+}
+
+string Investigador::getInstitucion() const { 
+    return Institucion; 
+}
+
+vector<Publicacion*> Investigador::getPublicaciones() const { 
+    return Publicaciones; 
+}
+
+// Setters
+void Investigador::setORCID(const string& orcid) { 
+    this->ORCID = orcid; 
+}
+
+void Investigador::setNombre(const string& nombre) { 
+    this->Nombre = nombre; 
+}
+
+void Investigador::setInstitucion(const string& institucion) { 
+    this->Institucion = institucion; 
+}
