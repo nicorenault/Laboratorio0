65a19c89282025acfa16267de78de36f9f2ad66b
vanemercader <vanessa.mercader@gmail.com>
2025-03-20 17:11:54 -0300

    Update DTRefer.cpp

--- src/DTRefer.cpp
+++ src/DTRefer.cpp
@@ -24,3 +24,17 @@ string DTRefer::getFecha() const {
 vector<string> DTRefer::getAutores() const { 
     return Autores; 
 }
+
+int DTRefer::getNumAutores() const { 
+    return Autores.size(); 
+}
+
+// Sobrecarga del operador << para imprimir DTRefer
+ostream& operator<<(ostream& os, const DTRefer& dt) {
+    os << dt.DOI << " -> " << dt.Titulo << " (" << dt.Fecha << ") / ";
+    for (size_t i = 0; i < dt.Autores.size(); ++i) {
+        os << dt.Autores[i];
+        if (i < dt.Autores.size() - 1) os << ", ";
+    }
+    return os;
+}
