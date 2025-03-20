2f8fb661b670d3c5905ca0333600e95c97535ae3
mateosalgado10 <mateosalgado100804@gmail.com>
2025-03-20 17:18:22 -0300

    Update PaginaWeb.cpp

--- src/PaginaWeb.cpp
+++ src/PaginaWeb.cpp
@@ -32,6 +32,13 @@ void PaginaWeb::setContenidoExtraido(string& contenidoExtraido) {
     this->ContenidoExtraido = contenidoExtraido; 
 }
 
-bool contienePalabra (string& palabra){
-    
+bool PaginaWeb::ContienePalabra(string& palabra) {
+    size_t pos = 0, found;
+    while ((found = this->ContenidoExtraido.find(' ', pos)) != string::npos) { //find busca los espacios
+        if (this->ContenidoExtraido.substr(pos, found - pos) == palabra) { // substr extrae la palabra que se encuentra entre pos y found
+            return true; // la palabra coincide
+        }
+        pos = found + 1; // se mueve al siguiente caracter despues del espacio
+    }
+    return (this->ContenidoExtraido.substr(pos) == palabra); // ultima palabra
 }
